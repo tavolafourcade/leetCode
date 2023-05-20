c4cd9de3cfab2e15c91da687ae84ecea7059a80b
tavolafourcade <tavolafourcade@gmail.com>
2023-05-20 11:31:11 -0500

    [ ADD ] 2. Remove Element solution

---
+++ 150-study-plan/2.removeElement.js
@@ -0,0 +1,24 @@
+/**
+ * TODO: Remove Element
+ * *Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ * *Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+ * *Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+ * *Return k.
+ */
+
+const removeElement = function(nums, val) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++){
+      if(nums[i] != val){
+          nums[count++] = nums[i]
+      }
+  }
+  return count
+};
+
+//TODO: Test Case
+const nums = [3,2,2,3]
+const val = 3
+
+const result = removeElement(nums,val)
+console.log(result) // 2
